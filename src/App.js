ac094de6c7f8a3b227f5f484dab505b4e2bc3add
Florencia Ferreyra Gre <flor.ferreyragre@gmail.com>
2020-07-24 11:16:28 -0300

    React - Unidad 2 - Lección 1 - Ejercicio 1

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    displayMessage: false,
+  };
+  render() {
+    return (
+      <div
+        className="App"
+        onClick={() => this.setState({ displayMessage: true })}
+      >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <b>Welcome to React</b>
+          </p>
+        </header>
+        <div className="div-body">
+          {this.state.displayMessage ? 'Este es el estado del componente' : ''}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,104 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import FormField from './FormField';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-ReactDOM.render(
-  <form id="form">
-    <h1 id="header">Registro Personal</h1>
-    <div id="content">
-      <FormField
-        fielContentId="name"
-        fieldName="Primer Nombre"
-        placeholderName="Primer Nombre"
-        borderRadius="input-group"
-        spanClass="input-group-text input-group input-group-prepend"
-        iconName="icon fa fa-user"
-        inputType="text"
-        inputClass="form-control input-group"
-      />
+class Text extends React.Component {
+  state = {
+    displayMessage: false,
+  };
 
-      <FormField
-        fielContentId="last-name"
-        fieldName="Apellido"
-        placeholderName="Apellido"
-        borderRadius="input-group"
-        spanClass="input-group-text input-group input-group-prepend"
-        iconName="icon fa fa-user"
-        inputType="text"
-        inputClass="form-control input-group"
-      />
-
-      <FormField
-        fielContentId="email"
-        fieldName="Correo Electrónico"
-        placeholderName="Correo Electrónico"
-        borderRadius="input-group"
-        spanClass="input-group-text input-group input-group-prepend"
-        iconName="icon fa fa-envelope"
-        inputType="text"
-        inputClass="form-control input-group"
-      />
-
-      <FormField
-        fielContentId="password"
-        fieldName="Contraseña"
-        placeholderName="Contraseña"
-        borderRadius="input-group"
-        spanClass="input-group-text input-group input-group-prepend"
-        iconName="icon fa fa-key"
-        inputType="password"
-        inputClass="form-control input-group"
-      />
-
-      <FormField
-        fielContentId="direction"
-        fieldName="Dirección"
-        placeholderName="Dirección"
-        borderRadius="input-group"
-        spanClass="input-group-text input-group input-group-prepend"
-        iconName="icon fa fa-home"
-        inputType="text"
-        inputClass="form-control input-group"
-      />
-
-      <FormField
-        fielContentId="city"
-        fieldName="Ciudad"
-        divInputId="city"
-        inputType="text"
-        inputClass="form-control"
-      />
-
-      <FormField
-        fielContentId="postcode"
-        fieldName="Código Postal"
-        divInputId="postcode"
-        inputType="text"
-        inputClass="form-control"
-      />
-    </div>
-    <div className="form-footer">
-      <div id="terms-accept">
-        <input id="check" type="checkbox" />
-        <label htmlFor="acepto-terminos">
-          {' '}
-          Estoy de acuerdo con el registro de datos personales
-        </label>
+  render() {
+    return (
+      <div
+        className="div-body"
+        onClick={() => this.setState({ displayMessage: true })}
+      >
+        {this.state.displayMessage ? 'Este es el estado del componente' : ''}
       </div>
-      <input
-        id="save-button"
-        className="form-control"
-        type="button"
-        value="Guardar"
-      />
-    </div>
-  </form>,
-  document.querySelector('#root'),
-);
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
 
 serviceWorker.unregister();
